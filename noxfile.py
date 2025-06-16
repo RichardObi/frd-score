7e5fe19293347627ce3d819f247c8d24a2f5775d
Richard Osuala <richardosuala@Richards-Air.fritz.box>
2025-06-17 01:55:18 +0200

    enabling tests for multiple python version

--- noxfile.py
+++ noxfile.py
@@ -8,8 +8,8 @@ import nox
 LOCATIONS = ("frd_v1/", "frd_v0/", "frd_v1/", "frd_v1/src/", "frd_v0/tests/", "frd_v0/src/", "frd_v0/tests/",  "src/", "tests/", "noxfile.py", "setup.py")
 
 
-@nox.session(python=["3.9"])
-#@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+#@nox.session(python=["3.9"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session):
     session.install("setuptools")
     session.install("numpy")
